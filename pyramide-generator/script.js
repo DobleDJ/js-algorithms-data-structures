594beed8cd9e55c04e140bd8fb5a90b9a153a844
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-11-09 19:28:59 -0500

    Initializejs file

---
+++ pyramide-generator/script.js
@@ -0,0 +1,11 @@
+console.log("Let's build a Pyramide Generator")
+console.log("    #    ")
+console.log("   ###   ")
+console.log("  #####  ")
+console.log(" ####### ")
+console.log("#########")
+
+// Variables
+let hello
+
+let character
