ce14406d927ebbcbdc7fc78777208898aa38679c
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-11-17 02:55:56 -0500

    Loop a triangle

--- eloquent-javascript/program-structure.js
+++ eloquent-javascript/program-structure.js
@@ -17,6 +17,13 @@ Write a loop that makes seven calls to console.log to output the following trian
 #######
 */
 
+let characterNumber = "#"
+let triangle = ""
+for (let index = 0; index < 7; index++) {
+  triangle += characterNumber
+  console.log(triangle)
+}
+
 /*
 2- FizzBuzz
 Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
