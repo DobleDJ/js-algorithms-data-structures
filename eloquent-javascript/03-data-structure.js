447e2a301cd05f6798f4b32209257b2ef206201e
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-11-20 14:24:02 -0500

    Como asignación adicional, modifica tu función range para que tome un tercer argumento opcional que indique el valor de paso utilizado al construir el array. Si no se proporciona un paso, los elementos deberían aumentar en incrementos de uno, correspondiendo al comportamiento anterior.

--- eloquent-javascript/03-data-structure.js
+++ eloquent-javascript/03-data-structure.js
@@ -54,3 +54,44 @@ let sum = (array) => {
 }
 
 console.log(sum(rangeNumber(1, 10)))
+
+/**
+ * 3- Como asignación adicional, modifica tu función range para que tome un
+tercer argumento opcional que indique el valor de “paso” utilizado al construir
+el array. Si no se proporciona un paso, los elementos deberían aumentar en
+incrementos de uno, correspondiendo al comportamiento anterior.
+ */
+
+/**
+ *
+ * @param {number} inicio initial value of array
+ * @param {number} fin final value of array
+ * @param {number} paso step to numbers if omited paso is equal 1
+ * @returns Returns an array from start to end arg
+ */
+let rangeNumber2 = (inicio, fin, paso = 1) => {
+  let rangeArray = []
+  if (!Number.isInteger(inicio) || !Number.isInteger(fin)) throw new Error("Only numbers allowed")
+  //if (inicio < 0 || fin < 0) throw new Error("Only integer positive numbers")
+  if (inicio > fin && paso > 0) {
+    for (let index = inicio; index >= fin; index -= paso) {
+      rangeArray.push(index)
+    }
+  } else if (inicio > fin && paso < 0) {
+    //incremento negativo
+    for (let index = inicio; index >= fin; index = index + paso) {
+      rangeArray.push(index)
+    }
+  } else {
+    for (let index = inicio; index <= fin; index += paso) {
+      rangeArray.push(index)
+    }
+  }
+  return rangeArray
+}
+
+try {
+  console.log(rangeNumber2(5, 2, -1))
+} catch (error) {
+  console.error(error.message)
+}
