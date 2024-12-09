5a57405df87da93cf042d5f0a45c0998af8a862d
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-12-09 00:47:30 -0500

    Pyramide generator exercise #00 - javaScript

--- pyramide-generator/script.js
+++ pyramide-generator/script.js
@@ -5,7 +5,44 @@ console.log("  #####  ")
 console.log(" ####### ")
 console.log("#########")
 
-// Variables
-let hello
+/**
+ * Create a pyramide with character and the count of pyramide lines
+ * @param {string} char Any character to build a pyramide
+ * @param {number} count The count of pyramide lines
+ * @returns Returns a pyramide array
+ */
+const createPyramide = (char, count) => {
+  const pyramide = []
+  for (let index = 0; index < count; index++) {
+    pyramide.push(pyramideLine(char, count, index))
+  }
+  return pyramide
+}
 
-let character
+/**
+ *
+ * @param {string} char Any character to build a pyramide
+ * @param {number} count The count of pyramide lines
+ * @param {number} line The position line of the pyramide
+ * @returns Returns string line of the pyramide
+ */
+const pyramideLine = (char, count, line) => {
+  let emptyLine = " "
+  return emptyLine.repeat(count - line) + char.repeat(2 * line + 1) + emptyLine.repeat(count - line)
+}
+
+/**
+ * Convert a pyramide array into string
+ * @param {array} array Array of pyramide lines
+ * @returns Returns a pyramide string
+ */
+const showPyramide = (array) => {
+  let pyramideStr = ""
+  for (let values of array) {
+    pyramideStr += values + "\n"
+  }
+  return pyramideStr
+}
+
+const pyramideNew = createPyramide("#", 10)
+console.log(showPyramide(pyramideNew))
