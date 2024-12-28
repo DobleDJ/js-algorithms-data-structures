7d38d895145167517ff8b605db7a0f0e45a85eae
Yoandy Doble Herrera <yoandy.doble@gmail.com>
2024-12-27 21:15:57 -0500

    #02 - JavaScript feature studentMsg student's result exam

--- gradebook/gradebook-app.js
+++ gradebook/gradebook-app.js
@@ -121,3 +121,36 @@ function hasPassingGrade(score) {
 console.log(hasPassingGrade(100))
 console.log(hasPassingGrade(53))
 console.log(hasPassingGrade(87))
+
+/**
+ * Task 4
+ * Now that the teacher has all of the information they need,
+ * they want to be able to message the student with the results.
+ * Complete the studentMsg function with totalScores and studentScore for parameters.
+ * The function should return a string representing a message to the student.
+ *
+ * If the student passed the course, the string should follow this format:
+ * Class average: average-goes-here. Your grade: grade-goes-here. You passed the course.
+ *
+ * If the student failed the course, the string should follow this format:
+ * Class average: average-goes-here. Your grade: grade-goes-here. You failed the course.
+ *
+ * Replace average-goes-here with the average of the total scores.
+ * Replace grade-goes-here with the student's grade.
+ */
+
+/**
+ * Student's result exam
+ * @param {number} totalScores Average student score
+ * @param {number} studentScore Student score
+ * @returns Return a string representing a message to the student
+ */
+function studentMsg(totalScores, studentScore) {
+  if (hasPassingGrade(studentScore)) {
+    return `Class average: ${getAverage(totalScores)}. Your grade: ${getGradeII(studentScore)}. You passed the course.`
+  } else {
+    return `Class average: ${getAverage(totalScores)}. Your grade: ${getGradeII(studentScore)}. You failed the course.`
+  }
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37))
