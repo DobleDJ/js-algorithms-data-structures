573ed42864bec96797cad623b42de00db26d17fd
DobleDJ <yoandy.doble@gmail.com>
2025-01-07 23:06:30 -0600

    Feature sellweapon

--- dragon-game/modules/script.js
+++ dragon-game/modules/script.js
@@ -140,7 +140,17 @@ function buyWeapon() {
 /**
  * Vender las armas viejas por 15 de oro
  */
-function sellWeapon() {}
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15
+        goldText.innerText = gold
+        let currentWeapon = inventory.shift()
+        text.innerText = "You sold a " + currentWeapon + "."
+        text.innerText += " In your inventory you have: " + inventory
+    } else {
+        text.innerText = "Don't sell your only weapon!"
+    }
+}
 
 function fightSlime() {}
 
